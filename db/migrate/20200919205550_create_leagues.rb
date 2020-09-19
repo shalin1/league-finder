e6872b1269227ecd61303a5e4a7d6c72ae944428
Shalin Scupham <shalin.scupham@gmail.com>
2020-09-19 17:07:10 -0400

    Initial commit, scaffolding for leagues

---
+++ db/migrate/20200919205550_create_leagues.rb
@@ -0,0 +1,12 @@
+class CreateLeagues < ActiveRecord::Migration[6.0]
+  def change
+    create_table :leagues do |t|
+      t.string :name, presence: true
+      t.integer :price, presence: true, numericality: {only_integer: true}
+      t.decimal :lat, presence: true, precision: 10, scale: 6
+      t.decimal :long, presence: true, precision: 10, scale: 6
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/leagues_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class LeaguesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @league = leagues(:one)
+  end
+
+  test "should get index" do
+    get leagues_url, as: :json
+    assert_response :success
+  end
+
+  test "should create league" do
+    assert_difference('League.count') do
+      post leagues_url, params: { league: { lat: @league.lat, long: @league.long, name: @league.name, price: @league.price } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show league" do
+    get league_url(@league), as: :json
+    assert_response :success
+  end
+
+  test "should update league" do
+    patch league_url(@league), params: { league: { lat: @league.lat, long: @league.long, name: @league.name, price: @league.price } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy league" do
+    assert_difference('League.count', -1) do
+      delete league_url(@league), as: :json
+    end
+
+    assert_response 204
+  end
+end
